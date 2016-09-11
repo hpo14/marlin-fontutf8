45f84153760808748727810ca81036a54a20eb19
yhfudev <yhfudev@gmail.com>
2016-09-11 08:56:13 -0400

    update script

--- genallfont.sh
+++ genallfont.sh
@@ -5,7 +5,7 @@ DN_WORK=./tmp1
 DN_EXEC=../buildroot/share/fonts/
 (cd ${DN_EXEC}; gcc -o genpages genpages.c getline.c)
 
-LANGS="cn jp ru"
+LANGS="zh_CN zh_TW jp ru de"
 
 for LANG in ${LANGS} ; do
     rm -rf ${DN_WORK}/
@@ -13,7 +13,7 @@ for LANG in ${LANGS} ; do
     cp testlcdprintmain.cpp ${DN_WORK}/
     cp language_${LANG}.h ${DN_WORK}/
     cd ${DN_WORK}/
-    ../../buildroot/share/fonts/uxggenpages.sh
+    ../../buildroot/share/fonts/uxggenpages.sh ../../buildroot/share/fonts/ISO10646-0-3.bdf
     cd ../
     mv ${DN_WORK}/fontutf8-data.h language_data_${LANG}.h
     rm -rf ${DN_WORK}/
